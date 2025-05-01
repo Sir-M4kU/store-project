531710b18310de1ebb2075060b67181cd9500399
Camilo <sirm4ku@gmail.com>
2025-05-01 08:18:10 -0500

    remove stripe dependency

--- server/src/api/product/services/product.ts
+++ server/src/api/product/services/product.ts
@@ -3,21 +3,5 @@
  */
 
 import { factories } from '@strapi/strapi';
-import { stripe } from '../../../stripe';
 
-export default factories.createCoreService('api::product.product', ({ strapi: _strapi }) => ({
-  async createStripeProduct({ id, name, amount }: { id: string; name: string; amount: number }) {
-    const product = await stripe.products.create({
-      name,
-      id,
-      default_price_data: {
-        currency: "usd",
-        unit_amount: amount * 100
-      }
-    })
-
-    console.log("stripe product created")
-
-    return product
-  }
-}));
+export default factories.createCoreService('api::product.product');
